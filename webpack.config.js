0ab6987cff2befdba5737575cb1756479aba9aea
Billions Joel <joelatugonza@gmail.com>
2022-01-26 10:19:19 +0300

    Install htmlwebpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/js/index.js',
-  output: {
-    filename: 'js/bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+	entry: './src/js/index.js',
+	output: {
+		filename: 'js/bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: 'src/index.html',
+		}),
+	],
 };
