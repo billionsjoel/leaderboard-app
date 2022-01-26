8f7b65a343af82fc9aa43d097d488a49ee4793d0
Billions Joel <joelatugonza@gmail.com>
2022-01-26 15:32:57 +0300

    Install webpack path import resolver

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,31 @@
-import { resolve } from 'path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-export const entry = './src/index.js';
-export const output = {
-	filename: 'bundle.js',
-	path: resolve(__dirname, 'dist'),
-	clean: true,
-};
-export const plugins = [
-	new HtmlWebpackPlugin({
-		filename: 'index.html',
-		template: 'src/index.html',
-	}),
-];
-export const module = {
-	rules: [
-		{
-			test: /\.scss$/i,
-			exclude: /node_modules/,
-			use: ['style-loader', 'css-loader', 'sass-loader'],
-		},
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: 'src/index.html',
+		}),
 	],
-};
-export const devServer = {
-	static: './dist',
-	compress: true,
-	port: 8080,
+	module: {
+		rules: [
+			{
+				test: /\.scss$/i,
+				exclude: /node_modules/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+		],
+	},
+	devServer: {
+		static: './dist',
+		compress: true,
+		port: 8080,
+	},
 };
