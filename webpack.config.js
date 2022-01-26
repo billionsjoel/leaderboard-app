d2c0b38ce4943bbf4985787ab15546875f4d9dea
Billions Joel <joelatugonza@gmail.com>
2022-01-26 14:39:42 +0300

    Add path resolver

--- webpack.config.js
+++ webpack.config.js
@@ -2,28 +2,29 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'src/index.html',
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.scss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-    ],
-  },
-  devServer: {
-    static: './dist',
-    compress: true,
-    port: 8080,
-  },
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: 'src/index.html',
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.scss$/i,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+		],
+	},
+	devServer: {
+		static: './dist',
+		compress: true,
+		port: 8080,
+	},
+	resolve: { extensions: ['.js', '.ts'] },
 };
