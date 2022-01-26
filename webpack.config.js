1bce032ef6fc49673ad14a5be8963daf87c9abab
Billions Joel <joelatugonza@gmail.com>
2022-01-26 14:19:06 +0300

    Convert weback to es module

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,27 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { resolve } from 'path';
 
-module.exports = {
-  entry: './src/js/index.js',
-  output: {
-    filename: 'js/bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'src/index.html',
-    }),
+export const entry = './src/js/index.js';
+export const output = {
+  filename: 'js/bundle.js',
+  path: resolve(__dirname, 'dist'),
+};
+export const plugins = [
+  new HtmlWebpackPlugin({
+    filename: 'index.html',
+    template: 'src/index.html',
+  }),
+];
+export const module = {
+  rules: [
+    {
+      test: /\.scss$/i,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+    },
   ],
-  module: {
-    rules: [
-      {
-        test: /\.scss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-    ],
-  },
-  devServer: {
-    static: './dist',
-    compress: true,
-    port: 8080,
-  },
+};
+export const devServer = {
+  static: './dist',
+  compress: true,
+  port: 8080,
 };
