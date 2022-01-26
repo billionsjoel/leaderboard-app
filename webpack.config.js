f56515df556f42601b3cc4c1fadf01c364de5e12
Billions Joel <joelatugonza@gmail.com>
2022-01-26 09:59:39 +0300

    Install webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-	entry: './src/js/index.js',
-	output: {
-		filename: 'js/bundle.js',
-		path: path.resolve(__dirname, 'dist'),
-	},
+  entry: './src/js/index.js',
+  output: {
+    filename: 'js/bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 };
