e96f91048100fd4965967baad928446b9c7ba3d9
Billions Joel <joelatugonza@gmail.com>
2022-01-26 16:58:33 +0300

    Install webpack path import resolver

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
@@ -10,8 +11,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'src/index.html',
+      template: './src/index.html',
     }),
   ],
   module: {
