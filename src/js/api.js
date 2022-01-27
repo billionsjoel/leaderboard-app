b36671f639c9d56a05829a44f2390b0f999d0d9c
Billions Joel <joelatugonza@gmail.com>
2022-01-27 18:14:57 +0300

    Fix linter

--- src/js/api.js
+++ src/js/api.js
@@ -1,28 +1,28 @@
 const uniqueID = 'AIzaSyClzfrOzB818x55FASHvX4JuGQciR9lv';
 
 const postScores = async (score) => {
-	const response = await fetch(
-		`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${uniqueID}/scores`,
-		{
-			method: 'POST',
-			headers: { 'content-type': 'application/json' },
-			body: JSON.stringify(score),
-		}
-	);
+  const response = await fetch(
+    `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${uniqueID}/scores`,
+    {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(score),
+    },
+  );
 
-	const { result } = await response.json();
+  const { result } = await response.json();
 
-	return result;
+  return result;
 };
 
 const getScores = async () => {
-	const response = await fetch(
-		`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${uniqueID}/scores`
-	);
+  const response = await fetch(
+    `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${uniqueID}/scores`,
+  );
 
-	const { result } = await response.json();
+  const { result } = await response.json();
 
-	return result;
+  return result;
 };
 
 export { postScores, getScores };
