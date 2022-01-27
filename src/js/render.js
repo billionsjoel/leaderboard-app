ba518afdd8dc95bce7df6b93ece7a2fc3edfe604
Billions Joel <joelatugonza@gmail.com>
2022-01-27 13:51:36 +0300

    Add styling to leaderboard

--- src/js/render.js
+++ src/js/render.js
@@ -1,24 +1,24 @@
 const renderScores = (scores) => {
-  const UI = document.querySelector('.scores ul');
-  UI.innerHTML = '';
+	const UI = document.querySelector('.scores ul');
+	UI.innerHTML = '';
 
-  if (!scores.length) {
-    UI.innerHTML = '<li>No Scores Yet!</li>';
-    return -1;
-  }
+	if (!scores.length) {
+		UI.innerHTML = '<li>No Scores Yet!</li>';
+		return -1;
+	}
 
-  scores
-    .sort((a, b) => b.score - a.score)
-    .forEach((score, bg) => {
-      if (bg % 2 === 0) {
-        bg = 1;
-      } else {
-        bg = 2;
-      }
-      UI.innerHTML += `<li class="score-${bg}" id="${score.index}"><span>${score.user} :</span><span>${score.score}</span></li>`;
-    });
+	scores
+		.sort((a, b) => b.score - a.score)
+		.forEach((score, bg) => {
+			if (bg % 2 === 0) {
+				bg = 1;
+			} else {
+				bg = 2;
+			}
+			UI.innerHTML += `<li class="score-${bg}" id="${score.index}"><img src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50" class="avatar"><span>${score.user} :</span><span>${score.score}</span></li>`;
+		});
 
-  return scores;
+	return scores;
 };
 
 export default renderScores;
--- webpack.config.js
+++ webpack.config.js
@@ -2,28 +2,34 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, './dist'),
-    clean: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.scss$/i,
-        exclude: /node_modules/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-    ],
-  },
-  devServer: {
-    static: './dist',
-  },
+	mode: 'development',
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, './dist'),
+		clean: true,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.scss$/i,
+				exclude: /node_modules/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+			{
+				test: /\.(jpg|png)$/,
+				use: {
+					loader: 'url-loader',
+				},
+			},
+		],
+	},
+	devServer: {
+		static: './dist',
+	},
 };
